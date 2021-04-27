ce28ebad7029d233778a76f207d8f7822fb44bc3
Yosef <shmuli5770@gmail.com>
2021-04-27 10:00:43 -0400

    commit tree a7a91b3af0eaba88c54d9da42c2b9c47ba1af3c0

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,20 @@
+import { Component } from "react";
+import React from 'react';
+
 // your Bomb code here!
+export default class Bomb extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.secondsLeft > 0 ? `${this.state.secondsLeft} seconds left before I go boom!` : 'Boom!'}
+            </div>
+        )
+    }
+}
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,20 @@
+import { Component } from "react";
+import React from 'react';
+
 // your ImageSlider code here!
+export default class ImageSlider extends Component{
+    constructor() {
+        super()
+        this.state = {
+            currentSlideIndex: 0
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                I am on slide {this.state.currentSlideIndex}
+            </div>
+        )
+    }
+}
